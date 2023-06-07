a24b23f6ee6d3b7bcfceedf84715eac2b943e734
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-07 14:46:23 +0200

    init commit

---
+++ src/org/learning/java/library/Book.java
@@ -0,0 +1,47 @@
+package org.learning.java.library;
+
+public class Book {
+  private String title;
+  private int pages;
+  private String author;
+  private String editor;
+  
+  public Book(String title, int pages, String author, String editor) {
+    this.title = title;
+    this.pages = pages;
+    this.author = author;
+    this.editor = editor;
+  }
+  
+  public String getTitle() {
+    return title;
+  }
+  
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  
+  public int getPages() {
+    return pages;
+  }
+  
+  public void setPages(int pages) {
+    this.pages = pages;
+  }
+  
+  public String getAuthor() {
+    return author;
+  }
+  
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+  
+  public String getEditor() {
+    return editor;
+  }
+  
+  public void setEditor(String editor) {
+    this.editor = editor;
+  }
+}
---
+++ src/org/learning/java/library/Library.java
@@ -0,0 +1,7 @@
+package org.learning.java.library;
+
+public class Library {
+  public static void main(String[] args) {
+  
+  }
+}
